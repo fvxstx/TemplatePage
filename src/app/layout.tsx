c0f1d16afb59efb54ed9091cb92c2eba4d02fb0e
fvxstx <faut.btorres@gmail.com>
2024-10-11 13:56:50 -0300

    fix: meta data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,8 @@ import "../styles/globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "PayCoin",
-  description: "PayCoin",
+  title: "Prudent Invest",
+  description: "Prudent Invest",
 };
 
 export default function RootLayout({
